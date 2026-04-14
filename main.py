0e89db0c37ee507d9d4606ff61fd342d73dfa651
ApathyMoon <masternyada42@gmail.com>
2026-04-13 23:00:40 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Authors: Jeffery Gothmann, Daniel Hernandez, Joshua Moellers
+The code for Running the balatro simulations and also for testing the simulations
+'''
+import pydirectinput
+
+if __name__ == "__main__":
+    pass
