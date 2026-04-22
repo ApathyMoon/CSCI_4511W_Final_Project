c1938a879da1c6d4f3b05e30b2376f4a2e4d45bb
ApathyMoon <masternyada42@gmail.com>
2026-04-22 08:57:37 -0500

    Successfully built pylatro(balatro-rs) module for running Game Engine

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 Authors: Jeffery Gothmann, Daniel Hernandez, Joshua Moellers
 The code for Running the balatro simulations and also for testing the simulations
 '''
-import pydirectinput
+import pylatro
 
 if __name__ == "__main__":
-    pass
+    game = pylatro.GameEngine()
+
+    current_state = game.state
+    
+    print(current_state)
+'''
+    while not game.is_over:
+        pass
+'''
